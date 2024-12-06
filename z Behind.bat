77237144249aae80d97dff56a426c737f30dd84a
kamin1ii <90837199+kamin1ii@users.noreply.github.com>
2024-12-05 18:04:53 -0700

    Delete z Behind.bat

--- z Behind.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title stat behind
-:loop
-cls
-mpv --network-timeout=2 --volume=0 --osc=no --window-minimized=no --screen=1 --title="Behind" http://streetcatpull.hellobike.com/live/4258783365322591678_2.m3u8
-echo MPV has exited. Restarting in 2 seconds...
-timeout /t 2 /nobreak >nul
-goto loop
