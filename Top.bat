e5adeb015b24be9393a734fd8379bf4e89c9815d
kamin1ii <90837199+kamin1ii@users.noreply.github.com>
2024-12-05 18:04:33 -0700

    Delete Top.bat

--- Top.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title stat top
-:loop
-cls
-mpv --network-timeout=2 --volume=0 --screen=1 --window-minimized=yes --osc=no --window-minimized=no  --title="Top" http://streetcatpull.hellobike.com/live/4258783365322591678_1.m3u8
-echo MPV has exited. Restarting in 2 seconds...
-timeout /t 2 /nobreak >nul
-goto loop
