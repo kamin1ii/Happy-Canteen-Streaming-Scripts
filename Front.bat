7a9cc2900f32990bc18eced5131c2f1c197066c4
kamin1ii <90837199+kamin1ii@users.noreply.github.com>
2024-12-05 18:04:27 -0700

    Delete Front.bat

--- Front.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title stat front
-:loop
-cls
-mpv --network-timeout=2 --volume=100 --screen=1 --window-minimized=no --osc=no --title="Front" http://streetcatpull.hellobike.com/live/4258783365322591678_0.m3u8
-echo MPV has exited. Restarting in 2 seconds...
-timeout /t 2 /nobreak >nul
-goto loop
