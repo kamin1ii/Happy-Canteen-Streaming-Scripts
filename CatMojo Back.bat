9574257dc6282d4c69ad2ea6a9e7b0b9e9fbde42
kamin1ii <90837199+kamin1ii@users.noreply.github.com>
2024-12-05 18:04:15 -0700

    Delete CatMojo Back.bat

--- CatMojo Back.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title stat catmojoback
-:loop
-cls
-mpv --network-timeout=2 --volume=0 --osc=no --screen=1 --window-minimized=no --title="Cat Back" http://streetcatpull.hellobike.com/live/5144313095337151915_2.m3u8
-echo MPV has exited. Restarting in 2 seconds...
-timeout /t 2 /nobreak >nul
-goto loop
